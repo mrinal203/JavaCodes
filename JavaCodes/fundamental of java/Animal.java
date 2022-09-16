93f1a4327a652ff18c1caa31c0628b4d2dc5518f
mrinal202 <msauraj9@gmail.com>
2022-09-16 17:24:43 +0530

    Add

---
+++ JavaCodes/fundamental of java/Animal.java
@@ -0,0 +1,13 @@
+// public class Animal {
+  
+// }
+package p1;
+public class Animal {
+	
+	void pair() {
+		System.out.println("4 pair");
+	}
+	void food() {
+		System.out.println("grass");
+	}
+}
