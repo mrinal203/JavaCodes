9c98ea64ae7a00c23ddf6b11a70b39240dd6c371
mrinal202 <msauraj9@gmail.com>
2022-10-16 23:49:22 +0530

    add

---
+++ JavaCodes/java_oops/COLLECTION/LIST/Sorting.java
@@ -0,0 +1,16 @@
+package java_oops.COLLECTION.LIST;
+
+import java.util.Collections;
+import java.util.*;
+public class Sorting {
+    public static void main(String[] args) {
+         
+        ArrayList a = new ArrayList();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.add(40);
+        Collections.sort(a);
+
+    }
+}
