91d9808eda82f72a263c57b57dded343dc57497f
mrinal202 <msauraj9@gmail.com>
2022-10-20 00:44:00 +0530

    Add

--- JavaCodes/BasicJava/Q46.java
+++ JavaCodes/BasicJava/Q46.java
@@ -1,6 +1,8 @@
 package BasicJava;
 // Q46. Define a java method to convert decimal to binary & decimal to octal and decimal to hexadecimal without using inbuilt methods?
 
+import java.util.Scanner;
+
 // Explanation: You have to take input from user as integer and perform operation to convert it into binary and octal and hexadecimal.
 
 // For converting to binary divide it by 2 and take remainder.
@@ -9,5 +11,51 @@ package BasicJava;
 
 // For converting to hexadecimal divide it by 16 and take remainder(use A to F for 10 to 15).
  public class Q46 {
-    
+    //decimal to binary
+    static String Dec_to_Binary(int n){
+        String st = "";
+        do{
+            int rem = n%2;
+            st = rem+st;
+            n = n/2;
+
+        }while(n!=0);
+        return st;
+    }
+    //decimal to octal
+    static String Dec_to_Octal(int n){
+        String st = "";
+        do{
+            int rem = n%8;
+            st = rem+st;
+            n = n/8;
+
+        }while(n!=0);
+        return st;
+    }
+    //decimal to hexadecimal
+    static String Dec_to_hexadecimal(int n){
+        String st = "";
+        String str[]= {"A","B","C","D","E"}
+        
+        do{
+            int rem = n%16;
+            if(rem>9){
+                int i=rem%10;
+             st = str[i]+st;
+            }
+            else
+               st = rem+st;
+            n = n/16;
+
+        }while(n!=0);
+   return st;
+    } 
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int d  =scn.nextInt( Dec_to_Binary(d));
+        System.out.println(Dec_to_Octal(d));
+        System.out.println(Dec_to_hexadecimal(d));
+    }
 }
---
+++ JavaCodes/BasicJava/Q47.java
@@ -0,0 +1,12 @@
+package BasicJava;
+// Q47. Write a java program to print object of an class?
+
+// Explanation: Create Student class with custom constructor and after that print the properties of student object
+// not the address of object(by using over-riding concept).
+// Input: "Name" "id" "stream"
+// Output: Name id stream (Not the address of object).
+
+
+public class Q47 {
+    
+}
